2a0e5ccb16463bffb2557555606da2638e3152c1
Sma5aka <54479462+Sma5aka@users.noreply.github.com>
2022-07-07 16:12:22 +1000

    added flyout menu navigation

---
+++ WeatherHWG_new/FlyoutItemPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherHWG_new
+{
+    internal class FlyoutItemPage
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetPage { get; set; } 
+    }
+}
--- WeatherHWG_new/MainPage.xaml.cs
+++ WeatherHWG_new/MainPage.xaml.cs
@@ -8,11 +8,23 @@ using Xamarin.Forms;
 
 namespace WeatherHWG_new
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : FlyoutPage
     {
         public MainPage()
         {
             InitializeComponent();
+            flyout.listview.ItemSelected += OnSelectedItem;
+        }
+
+        private void OnSelectedItem(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutItemPage;
+            if(item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetPage));
+                flyout.listview.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
